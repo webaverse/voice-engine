db2ec9a0def89aac45099f140337b0e469b668b0
Avaer Kazmer <a@modules.io>
2023-01-24 11:44:49 -0500

    Add voice-constants.js

---
+++ voice-constants.js
@@ -0,0 +1,6 @@
+// voice packs
+export const voicePacksUrl = `https://webaverse.github.io/voicepacks/all_packs.json`;
+
+// voice endpoints
+export const voiceEndpointBaseUrl = `https://voice-cw.webaverse.com/tts`;
+export const voiceEndpointsUrl = `https://raw.githubusercontent.com/webaverse/tiktalknet/main/model_lists/all_models.json`;
