53728efb410a80b91d7d8172ea73e15a05074203
Avaer Kazmer <a@modules.io>
2023-01-24 11:47:57 -0500

    Update paths

--- voice-engine.js
+++ voice-engine.js
@@ -1,2 +1,3 @@
 export * from './voice-input/voice-input.js';
-export * from './voice-output/voice-output.js';
+export * from './voice-output/voice-pack-voicer.js';
+export * from './voice-output/voice-endpoint-voicer.js';
